caacc26fd03708ecf21e6f82d7e47ce0e86ea4e0
Ahmad Syamim <ahmad.syamim@gmail.com>
2021-09-29 20:03:22 +0900

    features : on enable, install all available theme

--- Providers/ModuleServiceProvider.php
+++ Providers/ModuleServiceProvider.php
@@ -5,6 +5,7 @@ namespace Modules\Theme\Providers;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Database\Eloquent\Factory;
 use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Storage;
 
 class ModuleServiceProvider
 {
@@ -33,6 +34,17 @@ class ModuleServiceProvider
     {
         // Check for theme resources
         File::copyDirectory(module_path($this->moduleName, 'Resources/themes'), storage_path('themes'));
+
+        $filenames = File::files(storage_path('themes'));
+
+        $packages = array_map(function ($filename) {
+            return basename($filename->getRelativePathname(), '.theme.tar.gz');
+        }, $filenames);
+
+        foreach ($packages as $package) {
+            \Artisan::call("theme:install", ['package' => $package]);
+        }
+
         return true;
     }
 }

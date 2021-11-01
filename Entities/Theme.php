f8741345c7a3287492fbc4e61ae33b86dfe4493e
Ahmad Syamim <ahmad.syamim@gmail.com>
2021-11-01 12:20:26 +0900

    fixes : fixed condition checking

--- Entities/Theme.php
+++ Entities/Theme.php
@@ -32,14 +32,14 @@ class Theme extends Model
         static::saving(function ($model) {
             if (preg_match('%^(?:(?:https?|ftp)://)(?:\S+(?::\S*)?@|\d{1,3}(?:\.\d{1,3}){3}|(?:(?:[a-z\d\x{00a1}-\x{ffff}]+-?)*[a-z\d\x{00a1}-\x{ffff}]+)(?:\.(?:[a-z\d\x{00a1}-\x{ffff}]+-?)*[a-z\d\x{00a1}-\x{ffff}]+)*(?:\.[a-z\x{00a1}-\x{ffff}]{2,6}))(?::\d+)?(?:[^\s]*)?$%iu', $model->url)) {
 
-            } else {
+            } else if ($model->url) {
                 // Not URL
                 $response = Http::get("https://packagist.org/search.json?q={$model->url}")->collect();
                 if ($response->get('total')) {
                     $results = Collect($response->get('results'))->first();
                     if ($results['name']) {
                         $responseGH = Http::get("https://raw.githubusercontent.com/{$results['name']}/master/composer.json")->collect();
-                        if ($responseGH->count()) {
+                        if ($responseGH->count() && $responseGH->get('title')) {
                             $model->title = $responseGH->get('title');
                             File::put("storage/themes/{$responseGH->get('title')}.theme.tar.gz",file_get_contents("https://raw.githubusercontent.com/{$results['name']}/master/dist/{$responseGH->get('title')}.theme.tar.gz"));
                         }
